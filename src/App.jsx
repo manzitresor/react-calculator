ad16fa3c18da8fd03c167b15f5658cd9c6ca293f
manzitresor <manzitre@gmail.com>
2024-07-29 11:32:47 +0200

    Add calculate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-export default function App() {
+import Calculate from "./component/Calculate";
+
+function App() {
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-400">
-      Hello world!
-      </h1>
+      <div className="h-screen w-96 bg-orange-400 mx-auto my-5">
+      <Calculate/>
+    </div>
     </>
-  );
+  )
 }
+
+export default App
---
+++ src/component/Calculate.jsx
@@ -0,0 +1,10 @@
+
+export default function Calculate() {
+  return (
+    <div>
+      <div className='h-16 w-full bg-gray-400 text-white'>
+        <h1>Hell0</h1>
+      </div>
+    </div>
+  )
+}
