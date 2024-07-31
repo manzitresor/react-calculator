fbd45d5a9b2e336cffa0ed8acf566ee94c968fa6
manzitresor <manzitre@gmail.com>
2024-07-31 11:27:39 +0200

    Add functionality

--- src/component/Buttons.jsx
+++ src/component/Buttons.jsx
@@ -1,12 +1,13 @@
 
 /* eslint-disable react/prop-types */
-export default function Buttons({index,button}) {
+export default function Buttons({index,button,handleClick}) {
   return (
     <div>
-      <div
+      <div  
             className={`h-24 border border-gray-500 text-4xl flex items-center justify-center cursor-pointer ${
             (index + 1) % 4 === 0 ? 'bg-orange-400' : 'bg-gray-300'
-              }`}> {button}
+              }`}
+              onClick={handleClick}> {button}
       </div>
     </div>
   )
--- src/component/Calculate.jsx
+++ src/component/Calculate.jsx
@@ -1,15 +1,71 @@
+import { useState } from "react";
 import Buttons from "./Buttons";
 
 export default function Calculate() {
     const buttons = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+    const [input,setInput] = useState('0')
+    const [prevInput,setPrevInput]= useState(0);
+    const [operation, setOperation] = useState(null)
+
+    function isNumber(item) {
+         return !!item.match(/[0-9]+/);
+    }
+    
+
+    function handleClick(event){
+        const textData = event.target.innerText;
+
+        if(textData === 'AC') {
+            setInput(0);
+            setPrevInput(0);
+            setOperation(null);
+        }
+        else if(isNumber(textData)){
+            setInput(prev=>(
+                prev === 0 && textData === '0'? '0': Number(prev + textData) 
+            ))
+        } else if(textData  === '+/-') {
+            setInput(prev => -prev)
+        } else if(textData === '%') {
+            setInput(prev => prev / 100)
+        } else if((['+','-','x','÷']).includes(textData)) {
+            setPrevInput(input);
+            setInput('');
+            setOperation(textData)
+        } else if(textData === '=') {
+            let result;
+            if(prevInput && operation !== null) {
+                switch(operation) {
+                    case '+':
+                        result = parseFloat(prevInput) + parseFloat(input);
+                    break; 
+                    case '-':
+                        result = parseFloat(prevInput) - parseFloat(input);
+                    break; 
+                    case 'x':
+                        result = parseFloat(prevInput) * parseFloat(input);
+                    break; 
+                    case '÷':
+                        result = parseFloat(prevInput) / parseFloat(input);
+                    break; 
+                }
+                setInput(result);
+                setOperation(null);
+                setPrevInput(null);
+            }
+           
+        }
+       
+        
+    }
     
     return (
         <div>
             <div className='h-16 w-full bg-gray-500 text-white'>
-                <h1 className="text-right text-5xl px-4 ">0</h1>
+                <h1 className="text-right text-5xl px-4 ">{input || prevInput}</h1>
                 <div className="grid grid-cols-4 w-full">
                     {buttons.map((button, index) => (
-                        <Buttons key={index} button={button} index={index}></Buttons>
+                        <Buttons key={index} button={button} index={index} handleClick={handleClick}></Buttons>
                     ))}
                 </div>
             </div>
